67aaf294a677816d94ea79dbc1d2f48259dbc8ef
Mike O'Brien <mikeo@10gen.com>
2013-07-12 16:12:37 -0400

    yeah

--- app/report_tasks.py
+++ app/report_tasks.py
@@ -2,9 +2,9 @@ from celery_conf import celery
 from report_generators import generate_plot_report
 
 @celery.task
-def run_report(file_id, report_type):
+def run_report(file_id, report_type, task_id=None):
     print "i am running the reportttttt"
-    generate_plot_report(file_id) # write result of the report
+    generate_plot_report(file_id, task_id) # write result of the report
 
 
 
