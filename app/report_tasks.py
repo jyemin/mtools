310c1555d714d2ad288a16426e4bd7cb21006735
Mike O'Brien <mikeo@10gen.com>
2013-07-12 14:31:55 -0400

    call report generator function from celery

--- app/report_tasks.py
+++ app/report_tasks.py
@@ -1,9 +1,14 @@
 from celery import Celery
+from report_generators import generate_plot_report
 
 celery = Celery('reports', broker='mongodb://localhost:27017/report_tasks')
 
 
 @celery.task
-def run_report(file, report_type):
-    print 'report: {0}'.format(report_type)
-    return 'wildcat'
+def run_report(file_id, report_type):
+    print "i am running the reportttttt"
+    generate_plot_report(file_id) # write result of the report
+
+
+
+    #print 'report: {0}'.format(report_type)
